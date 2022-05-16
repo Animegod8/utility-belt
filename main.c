f02fe2297b606c804aec02869bddd6e40cecbd29
Animegod8 <>
2022-05-16 13:57:20 +0000

    added info of project

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+
 #include "src/calculator.c"
 #include "src/excshell.c"
+
+void info(){
+    FILE* ptr;
+    char ch;
+ 
+    // Opening file in reading mode
+    ptr = fopen("info.txt", "r");
+ 
+    if (NULL == ptr) {
+        printf("file can't be opened, File an issue request at the github repo \n");
+    }
+ 
+    // Printing what is written in file
+    // character by character using loop.
+    do {
+        ch = fgetc(ptr);
+        printf("%c", ch);
+ 
+        // Checking if character is not EOF.
+        // If it is EOF stop eading.
+    } while (ch != EOF);
+ 
+    // Closing the file
+    fclose(ptr);
+}
+
+
 
 void prompt(){
   int choice;
@@ -9,8 +37,13 @@ void prompt(){
   scanf("%d", &choice);
   if(choice == 1){
     calculate();
+    printf("\n");
   } else if (choice == 2){
     commandPrompt();
+    printf("\n");
+  } else {
+    info();
+    printf("\n");
   }
 }
 
